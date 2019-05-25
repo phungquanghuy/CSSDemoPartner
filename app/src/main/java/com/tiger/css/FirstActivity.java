26d566b28ca34660f18cd9557867603fdfebb3dd
tranhuongk <hUONG123>
2019-05-25 18:15:37 +0700

    first blood

--- app/src/main/java/com/tiger/css/FirstActivity.java
+++ app/src/main/java/com/tiger/css/FirstActivity.java
@@ -3,6 +3,8 @@ package com.tiger.css;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import com.squareup.picasso.Picasso;
@@ -12,6 +14,7 @@ public class FirstActivity extends AppCompatActivity {
 
     private ImageView avatar;
     private SupportUser mSupportUser = new SupportUser();
+    Button active;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +27,29 @@ public class FirstActivity extends AppCompatActivity {
 
         avatar = (ImageView) findViewById(R.id.avatar);
         Picasso.get().load(mSupportUser.getUrl()).into(avatar);
+        active = (Button) findViewById(R.id.active);
 
+        active.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toggleBtn();
+            }
+        });
 
+    }
+
+    protected void toggleBtn(){
+        if(!mSupportUser.getStatus().equals("offline")){
+            mSupportUser.setStatus("offline");
+            active.setBackgroundResource(R.drawable.active_btn);
+            active.setTextColor(0xFFFFFFFF);
+            active.setText("Active");
+        }
+        else{
+            mSupportUser.setStatus("actived");
+            active.setBackgroundResource(R.drawable.offline_btn);
+            active.setTextColor(0xFF000000);
+            active.setText("Offlne");
+        }
     }
 }
